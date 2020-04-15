d56c9ddcb94710ce03b8e34b24b9436e510102bd
aritra363 <aritrapaul363@gmail.com>
2020-04-15 13:40:43 +0530

    Updates

---
+++ precache-manifest.2d05d4674a7f743ed18bc6146a754ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb71dc4254893e4b531ad183a09b67f",
+    "url": "/React_Recipe_App/index.html"
+  },
+  {
+    "revision": "43f964b92d7b9eb9fe07",
+    "url": "/React_Recipe_App/static/css/main.4b578cb1.chunk.css"
+  },
+  {
+    "revision": "411fd6d32eb77fb07c31",
+    "url": "/React_Recipe_App/static/js/2.d776df86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Recipe_App/static/js/2.d776df86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f964b92d7b9eb9fe07",
+    "url": "/React_Recipe_App/static/js/main.16275d7f.chunk.js"
+  },
+  {
+    "revision": "2f71191bba450e0cb4ef",
+    "url": "/React_Recipe_App/static/js/runtime-main.7bb53014.js"
+  }
+]);
